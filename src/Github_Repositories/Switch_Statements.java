cb889f5d29b2acb643e2d4c6bb43ef5a41151919
SananAbdulla <100719042+SananAbdulla@users.noreply.github.com>
2024-11-14 01:27:32 +0400

    initial Commit

--- src/Github_Repositories/Switch_Statements.java
+++ src/Github_Repositories/Switch_Statements.java
@@ -48,6 +48,9 @@ public class Switch_Statements {
             System.out.println("-------");
             System.out.println("-------");
 
+            System.out.println("-------");
+            System.out.println("-------");
+
 
 
 
