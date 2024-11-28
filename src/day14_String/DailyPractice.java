a8fb69161c2e28dde6f4f28d7e9055e80efaa9b8
SananAbdulla <sananabdulla@gmail.com>
2024-11-28 23:43:27 +0400

    initial Commit

--- src/day14_String/DailyPractice.java
+++ src/day14_String/DailyPractice.java
@@ -6,17 +6,37 @@ public class DailyPractice {
 
 
 
-        int num = 1000;
+//        int num = 1000;
+//
+//        System.out.println((num + "").length());
+//
+//
+//
+//        String str = "Loopcamp";
+//        String str2 = "LOOp";
+//
+//        System.out.println(str.equalsIgnoreCase("LOOpcamp"));
+//        System.out.println(str.toLowerCase().startsWith("loop".toLowerCase()));
 
-        System.out.println((num + "").length());
 
 
+//        String str = " Baku is the capital of Azerbaijan";
+//
+//
+//        System.out.println(str.contains("Baku"));
+//        System.out.println(str.contains("Baku, is"));
+//        System.out.println(str.contains("Baku") && str.contains("capital")); // remember this
+//
 
-        String str = "Loopcamp";
-        String str2 = "LOOp";
+String str = "    loop    ";
+String str1 = "Apple is good brand";
 
-        System.out.println(str.equalsIgnoreCase("LOOpcamp"));
-        System.out.println(str.toLowerCase().startsWith("loop".toLowerCase()));
+        //System.out.println(str.trim().length());
+        System.out.println(str);
+        str = str.trim();
+        System.out.println(str);
+        System.out.println("\"" + str + "\" sent!");
+
 
     }
 }
---
+++ src/day15_String/Practice.java
@@ -0,0 +1,18 @@
+package day15_String;
+
+public class Practice {
+
+
+    public static void main(String[] args) {
+
+
+
+
+        String str = "  Applee ";
+        char ch = 'A';
+
+//        System.out.println(str.trim().startsWith("A"));
+//        System.out.println((ch +"").startsWith('A'));
+
+    }
+}
