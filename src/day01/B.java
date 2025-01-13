236b19a092d600dfcb5f4e50d9b678c40b3d3e07
SananAbdulla <sananabdulla@gmail.com>
2025-01-13 21:17:07 +0400

    into to 2D Arrays

---
+++ src/day01/B.java
@@ -0,0 +1,7 @@
+package day01;
+
+public class B extends A {
+    public B() {
+        System.out.println("B");
+    }
+}
---
+++ src/day01/Test.java
@@ -0,0 +1,8 @@
+package day01;
+
+public class Test extends B {
+    public Test() {
+        System.out.println("C");
+    }
+
+}
